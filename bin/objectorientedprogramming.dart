baea14eed830300e3c7ae4bcdf8a3b25f099a28a
Valery Kuchuk <kuchuk-valerochka@mail.ru>
2023-10-31 00:46:51 +0300

    OOP

---
+++ bin/objectorientedprogramming.dart
@@ -0,0 +1,5 @@
+import 'package:objectorientedprogramming/objectorientedprogramming.dart' as objectorientedprogramming;
+
+void main(List<String> arguments) {
+  print('Hello world: ${objectorientedprogramming.calculate()}!');
+}
